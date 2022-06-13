92717719dd75ea90d7e446cf6c484c8cdd93b2db
rafalaranja <rafaelfsantos03@gmail.com>
2022-06-13 14:00:00 +0100

    Musica de Fundo a Funcionar

--- python/musicaFundo.py
+++ python/musicaFundo.py
@@ -1,30 +1,47 @@
 import sys
 import time
-from time import sleep
-import requests
+
+import requests ## biblioteca para os pedidos
+
 import winsound
 
-def play_sound(file):
-    winsound.PlaySound(file, winsound.SND_FILENAME)
-
+def play_sound(x):
+    winsound.PlaySound(x, winsound.SND_ALIAS | winsound.SND_NODEFAULT | winsound.SND_ASYNC)
+    
 try :
 
     print( "Prima CTRL+C para terminar")
+
     
 
     while True: # ciclo para o programa executar sem parar…
 
-        r=requests.get("http://localhost/TI/ProjetoTI_Fase2/api/api.php?nome=musica")
-        print(r.text)
-        if(r.text=='1'):
-            while(KeyboardInterrupt != 0):
-                play_sound('A_Portuguesa.wav')
-        elif(r.text=='0'):
-            play_sound(None)
+        cont=0
+
+        print("*** LER Estado do som no servidor ***")
+
+        r=requests.get('http://localhost/TI/ProjetoTI_Fase2/api/api.php?nome=musica&ficheiro=valor')  
+
+        if r.status_code==200:
+
+            
+            print(r.text)
+            if int(r.text) == 1:
+                play_sound("A_Portuguesa.wav")
+                while int(r.text)==1 and cont<290 :
+                    r=requests.get('http://localhost/TI/ProjetoTI_Fase2/api/api.php?nome=musica&ficheiro=valor')
+                    if int(r.text) == 1:
+                        cont=cont+1
+                        print(cont)
+                        time.sleep(1)
+            if int(r.text) == 0:
+                play_sound(None)
+                
+
         else:
             print("O pedido HTTP não foi bem sucedido")
-        
-        time.sleep (2)
+
+
 
 except KeyboardInterrupt: # caso haja interrupção de teclado CTRL+C
 
